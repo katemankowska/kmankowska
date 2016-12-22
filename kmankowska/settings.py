ce7bab417e2432c6fe682457b7a65c6a68eebd8b
Pawel Mankowski <pawel.mankowski@here.com>
2016-12-21 19:34:13 -0800

    adding gitignore and staticroot variable in settings

--- kmankowska/settings.py
+++ kmankowska/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -24,6 +24,8 @@ SECRET_KEY = '!v46p50#@evnffh!n3l3a)mv_@zvw#==yz(mybpfh(&!8(s0zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
 
 
 # Application definition
@@ -116,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'), )
 
