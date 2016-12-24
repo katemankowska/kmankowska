71f0e4207fde1389c9b8fdd9baf8a8301558e915
Pawel Mankowski <pawel.mankowski@here.com>
2016-12-24 11:41:53 -0800

    added some translation tags in templates. added pdf resume and updated info to reflect kmankowska resume

--- resume/urls.py
+++ resume/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from resume import views
 
 urlpatterns = [
-    url(r'^$', views.profile, name='index')
+    url(r'^$', views.profile, name='index'),
+    url(r'^resume', views.resume_pdf, name='resume_pdf')
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+import os
 
 
 def profile(request):
     return render(request, "resume/index.html")
+
+
+def resume_pdf(request):
+    with open(os.path.join(settings.BASE_DIR, 'static/resume/kmankowska_resume.pdf'), 'rb') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+        return response
